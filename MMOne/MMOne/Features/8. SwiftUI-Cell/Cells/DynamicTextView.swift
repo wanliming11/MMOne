2d9e53c39850a0a641081132774ec64d20f5fd61
wanliming <wanliming@douyu.tv>
2023-09-18 20:02:58 +0800

    Fixed warnings

--- MMOne/MMOne/Features/8. SwiftUI-Cell/Cells/DynamicTextView.swift
+++ MMOne/MMOne/Features/8. SwiftUI-Cell/Cells/DynamicTextView.swift
@@ -13,14 +13,55 @@ struct DynamicTextView: View {
     let maxLines = 6
 
     var body: some View {
-        Text(viewModel.content.trimmingCharacters(in: .whitespacesAndNewlines)).lineLimit(isExpand ? nil : maxLines)
+        Text(adjustContent(viewModel.content, maxLines)).lineLimit(isExpand ? nil : maxLines)
             .onTapGesture {
                 isExpand.toggle()
             }
+    }
+
+    private func adjustContent(_ content: String, _ limitedShowLine: Int) -> String {
+        var lines = content.components(separatedBy: .newlines)
+        guard lines.count >= limitedShowLine else { return content }
+
+        // rule1: if the last line is empty, remove it
+        if lines.count == limitedShowLine, lines.last?.isEmpty == true {
+            lines.removeLast()
+            let newText = lines.joined(separator: "\n")
+            return newText + "..."
+        }
+
+        if lines.count > limitedShowLine, lines[limitedShowLine - 1].isEmpty {
+            lines[5] = "..."
+        }
+
+        return lines.joined(separator: "\n")
     }
 }
 
 @available(iOS 17.0, *)
 #Preview {
-    DynamicTextView(viewModel: DynamicCellViewModel.TextViewModel(content: "这是一条文本"))
+    let inputText = """
+    Line 1
+    Line 2
+    Line 3
+    Line 4
+    Line 5
+    \n
+    """
+    return DynamicTextView(viewModel: DynamicCellViewModel.TextViewModel(content: inputText))
+}
+
+@available(iOS 17.0, *)
+#Preview {
+let inputText = """
+    Line 1
+    Line 2
+    Line 3
+    Line 4
+    Line 5
+    \n
+    \n
+    \n
+    """
+    return DynamicTextView(viewModel: DynamicCellViewModel.TextViewModel(content: inputText))
 }
